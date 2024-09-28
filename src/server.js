66c4760366e64d781bb83cecb61950fd884d31d4
Ahmad Mostafa <124518625+ahmadalasiri@users.noreply.github.com>
2024-09-28 23:21:10 +0300

    Initialize Express app with MongoDB connection and set up routes

---
+++ src/server.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const { connectDB } = require("./dbConnection");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const { authenticateToken, allowedTo } = require("./middleware/authMiddleware");
+
+connectDB().then(() => {
+  console.log("Connected to DB");
+});
+
+// Initialize Express app
+const app = express();
+app.use(cookieParser());
+
+// Middleware
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // To parse incoming JSON requests
+app.use(express.static("public")); // Serve static files
+
+// Connect to MongoDB
+
+// Routes
+
+app.use("/identify", authRoutes);
+app.use("/users", userRoutes);
+app.use("/admin", adminRoutes);
+app.get(
+  "/student1",
+  authenticateToken,
+  allowedTo("student1", "teacher", "admin"),
+  (req, res) => {
+    res.render("student1", { user: req.user });
+  }
+);
+app.get(
+  "/student2",
+  authenticateToken,
+  allowedTo("student2", "teacher", "admin"),
+  (req, res) => {
+    res.render("student2", { user: req.user });
+  }
+);
+
+app.get(
+  "/teacher",
+  authenticateToken,
+  allowedTo("teacher", "admin"),
+  (req, res) => {
+    res.render("teacher", { user: req.user });
+  }
+);
+app.get("/granted", authenticateToken, (req, res) => {
+  console.log("req.user", req.user);
+  res.render("granted", { user: req.user }); // Pass user info if needed
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("access_token");
+  res.redirect("/");
+});
+
+// Global error handling (optional)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(400).send(`${err.message}`);
+});
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
